177949e841e19b67d1aa664fe5f27c7eb1877fc6
Robin van der Weide <robinweide@gmail.com>
2016-09-14 14:35:12 +0200

    Added zlim-Coef and removed ggplot2::ggplotGrob()
     ggplot2::ggplotGrob() is not allways found... Check this future me

--- R/visualise.APA.ggplot.R
+++ R/visualise.APA.ggplot.R
@@ -5,7 +5,7 @@
 #' @param Focus Wich sample does need to be the to-compare sample?
 #' @return A grid object, containing two ggplot-objects.
 #' @export
-visualise.APA.ggplot <- function(APAlist, title, focus = 1){
+visualise.APA.ggplot <- function(APAlist, title, zCoef = 1, focus = 1, ...){
   # Make two dataframes
   abovePlots <- data.frame(Var1 = integer(),
                            Var2 = integer(),
@@ -19,14 +19,14 @@ visualise.APA.ggplot <- function(APAlist, title, focus = 1){
     a <- reshape2::melt(APAlist[[i]])
     a$sample <- factor(rep(names(APAlist)[i], length(a[,1])))
     abovePlots <- rbind(abovePlots, a)
-
+    
     e <- reshape2::melt(APAlist[[i]]-APAlist[[focus]])
     e$sample <- rep(paste0(names(APAlist)[focus], ' vs ', names(APAlist)[i] ) , length(e[,1]))
     belowPlots <- rbind(belowPlots, e)
     belownames <- c(belownames, paste0(names(APAlist)[focus], ' vs ', names(APAlist)[i] ))
   }
   # Find best color-limits
-  z <- max( abs( c(  quantile(na.exclude(abovePlots$value), .01) , quantile(na.exclude(abovePlots$value), .99)        )  )   )
+  z <- max( abs( c(  quantile(na.exclude(abovePlots$value), .01) , quantile(na.exclude(abovePlots$value), .99)        )  )   )*zCoef
   abovePlots$value[abovePlots$value > z] <- z
   abovePlots$value[abovePlots$value < (z *-1)] <- (z *-1)
   # Set focus-sample to column 1
@@ -41,7 +41,7 @@ visualise.APA.ggplot <- function(APAlist, title, focus = 1){
     ggplot2::labs(title = title,x = "", y = "", fill = "Contacts ") +
     ggplot2::scale_fill_distiller(limits = c(0, z), palette = "Spectral")
   # Find best color-limits
-  z2 <- max( abs( c(  quantile(na.exclude(belowPlots$value), .01) , quantile(na.exclude(belowPlots$value), .99)        )  )   )
+  z2 <- max( abs( c(  quantile(na.exclude(belowPlots$value), .01) , quantile(na.exclude(belowPlots$value), .99)        )  )   )*zCoef
   belowPlots$value[belowPlots$value > z2] <- z2
   belowPlots$value[belowPlots$value < (z2 *-1)] <- (z2 *-1)
   # Plot difference-plots
@@ -52,7 +52,7 @@ visualise.APA.ggplot <- function(APAlist, title, focus = 1){
     ggplot2::scale_x_continuous(breaks = c(5.5,11,16.5), labels = c("-50Kb", "3'", "+50Kb"))+
     ggplot2::scale_y_continuous(breaks = c(5.5,11,16.5), labels = c("-50Kb", "5'", "+50Kb")) +
     ggplot2::labs(x = "", y = "", fill = "Difference")
-  #return(grid.arrange(plot1, plot2, ncol=1))
-  grid::grid.newpage()
-  grid::grid.draw(rbind(grid::ggplotGrob(plot1), grid::ggplotGrob(plot2), size = "last"))
+  return(grid.arrange(plot1, plot2, ncol=1))
+  #grid::grid.newpage()
+  #grid::grid.draw(rbind(grid::ggplotGrob(plot1), grid::ggplotGrob(plot2), size = "last"))
 }

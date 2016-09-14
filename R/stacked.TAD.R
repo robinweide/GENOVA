2ff68bbfe78ef910bc7ae9ab78c69fab37086a11
Robin van der Weide <robinweide@gmail.com>
2016-09-14 14:38:55 +0200

    outliers, rawoutput, speed and bugs

--- R/stacked.TAD.R
+++ R/stacked.TAD.R
@@ -1,3 +1,5 @@
+
+
 #' Get a z-stack matrix of TADs
 #'
 #' Extracts matrices from a BED-like structure and resizes them, which leads to all start- and end-position of TADs overlapping.
@@ -7,9 +9,13 @@
 #' @param tad.bed Bedpe file containing the TAD positions: produced by read.bedpe(). Please make a bedpe, by copying tad-bed columns to columns 4,5 and 6!
 #' @param smallTreshold The minimal size of loops. Too small loops lead to messy plots.
 #' @param verbose Produces a progress-indication.
+#' @param saveRaw Logical: True will output the raw matrices per loop and performs outlier-detection.
+#' @param outlierCutOff The severity of outliers: roughly translates to the amount of MADs above the median.
 #' @return A matrix containing the Z-stack scores.
 #' @export
-stacked.TAD <- function(experiment, tad.bed, smallTreshold = 225e3, verbose = F){
+stacked.TAD <- function(experiment, tad.bed, smallTreshold = 225e3, verbose = F,saveRaw=T, outlierCutOff = 8){
+  MADTRESHOLD <- outlierCutOff
+  rawMatList = list()
   hicdata <- experiment$ICE
   bed <- experiment$ABS
   resolution <- experiment$RES
@@ -32,9 +38,9 @@ stacked.TAD <- function(experiment, tad.bed, smallTreshold = 225e3, verbose = F)
   # Make a bed from bedpe, with cols 1,2 and 6
   tad <- tad.bed[,c(1,2,6)]
   # Loop trough tads and sum over scorematrices
-  tad.length <- length(tad$V1)
+  tad.length <- length(tad[,1])
   # Add missing levels to Chromosomes
-  levels(tad$V1) <- levels(bed$V1)
+  levels(tad$V1) <- levels(bed[,1])
   # Successfull tads:
   SL <- 0
   for( i in 1:tad.length){
@@ -68,12 +74,34 @@ stacked.TAD <- function(experiment, tad.bed, smallTreshold = 225e3, verbose = F)
     newMat[lower.tri(newMat)] <- t(newMat)[lower.tri(newMat)]
     # Rescale to 100 breaks
     sel.resized <- resize.mat(newMat, c(100,100))
+    rawMatList[[i]] <- sel.resized
     if(!exists("results.vector")){
       results.vector <- sel.resized
     } else {
       results.vector <- results.vector + sel.resized
     }
     SL <- SL + 1
+  } 
+  if(saveRaw){
+    #return(list(STACK = (results.vector/SL)[1:99,1:99],RAW = simplify2array(rawMatList)))
+    rawMatList <- rawMatList[!unlist(lapply(rawMatList, is.null))]
+    sm <- simplify2array(rawMatList)
+    MED <- apply(sm,MARGIN = 1:2, median)
+    MAD <- apply(sm,MARGIN = 1:2, mad)
+    tres <- MED+(MAD*MADTRESHOLD)
+    tres[is.na(tres)] <- 0
+    for(i in 1:length(rawMatList)){
+      #rawMatList[[i]][rawMatList[[i]][1:99,1:99] > tres[1:99,1:99]] <- 0 #tres[rawMatList[[i]][1:99,1:99] > tres[1:99,1:99]]
+      m <- rawMatList[[i]]
+      #cat(i, "\n")
+      if(any(m[1:99,1:99] > tres[1:99,1:99]*5)){
+        rawMatList[[i]] <- matrix(0, nrow = 100, ncol=100)
+        SL - 1
+      }
+    }
+    STACKoutlierr <- Reduce(rawMatList, f = '+')
+    return(list(STACK = (results.vector/SL)[1:99,1:99],RAW = rawMatList, STACKoutlier =  (STACKoutlierr/SL)[1:99,1:99] )   )
+  }else{
+    return( (results.vector/SL)[1:99,1:99])
   }
-  return( (results.vector/SL)[1:99,1:99])
 }

56c4271405dd607da0091861dccf6f4ed0a329e9
Robin van der Weide <robinweide@gmail.com>
2016-09-19 15:34:29 +0200

    Added tests for good input data

--- R/construct.experiment.R
+++ R/construct.experiment.R
@@ -16,6 +16,16 @@ construct.experiment <- function(ICEDpath,BEDpath, SAMPLENAME, COLOR = 1, COMMEN
   
   ICE <- read.hicpro.matrix(ICEDpath)
   ABS <- data.table::fread(BEDpath, header = F, data.table = F)
+  
+  # check for similar binsizes BED and ICE
+  ICEmaxID <- max(max(ICE$V1), max(ICE$V2))
+  ABSmaxID <- max(ABS[,4])
+  if( ICEmaxID > ABSmaxID){
+    stop("Undefined HiC-bins.\nWe checked if the highest Hi-C bin in the Matrix-file could be found in the BED-file: it couldn't.\nPlease check if BED and Matrix are from the same reference genome and resolution!")
+  }
+  if( abs(ICEmaxID - ABSmaxID) > ICEmaxID*0.1 ) {
+    warning("Undefined BED-bins.\nWe checked if the highest Hi-C bin in the BED-file could be found in the Matrix-file: it couldn't.\nPlease check if BED and Matrix are from the same reference genome and resolution!")
+  }
   chromVector <- as.character(unique(ABS$V1))
   res = as.numeric( median(ABS$V3-ABS$V2)  )
   

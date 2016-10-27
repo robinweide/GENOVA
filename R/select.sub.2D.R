fc50e09eec184818d4159b9078649e538a6f5bb2
Robin van der Weide <robinweide@gmail.com>
2016-10-27 10:47:53 +0200

    Update select.sub.2D.R

--- R/select.sub.2D.R
+++ R/select.sub.2D.R
@@ -7,7 +7,6 @@
 #' @param Y The Y index-range.
 #' @return A data.table with normalised counts.
 #' @import data.table
-#' @export
 select.sub.2D <- function( data , X, Y ){
   x <- rep(X[1]:X[length(X)], X[length(X)]-X[1]+1)
   y <- rep(Y[1]:Y[length(Y)], each=length(Y[1]:Y[length(Y)]))

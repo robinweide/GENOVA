021f45ba08ddf001b1f95e562b285a41354617bb
Robin van der Weide <robinweide@gmail.com>
2016-10-27 11:55:09 +0200

    Update visualise.RCP.ggplot.R

--- R/visualise.RCP.ggplot.R
+++ R/visualise.RCP.ggplot.R
@@ -13,7 +13,7 @@ visualise.RCP.ggplot <-function(RCPdata){
     #geom_point(size = .25,alpha = .25) +
     ggplot2::geom_smooth(span = 0.25, se = F) +
     ggplot2::scale_x_log10() + ggplot2::scale_y_log10() +
-    ggplot2::facet_grid(. ~ chrom) +
+    ggplot2::facet_wrap( ~ chrom, ncol = floor(sqrt( length(unique(RCP_WTWAPL$chrom))  )) ) +
     ggplot2::theme_linedraw()+
     ggplot2::scale_color_manual(values = cols)+
     ggplot2::labs(title = "Relative contact probability", x = "Distance (bp)", y = "RCP", col = '')+

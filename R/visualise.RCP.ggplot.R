8e28c95714b7a07ff5088193b127d2d959b4c60f
Robin van der Weide <robinweide@gmail.com>
2016-10-27 15:35:31 +0200

    Update visualise.RCP.ggplot.R

--- R/visualise.RCP.ggplot.R
+++ R/visualise.RCP.ggplot.R
@@ -6,16 +6,14 @@
 #' @return A ggplot-object
 #' @export
 visualise.RCP.ggplot <-function(RCPdata){
-  cols <- RCPdata$color
-  names(cols) <- RCPdata$sample
-
-  ggplot2::ggplot(RCPdata, ggplot2::aes(col = sample,x = distance, y = prob)) +
-    #geom_point(size = .25,alpha = .25) +
-    ggplot2::geom_smooth(span = 0.25, se = F) +
-    ggplot2::scale_x_log10() + ggplot2::scale_y_log10() +
-    ggplot2::facet_wrap( ~ chrom, ncol = floor(sqrt( length(unique(RCP_WTWAPL$chrom))  )) ) +
-    ggplot2::theme_linedraw()+
-    ggplot2::scale_color_manual(values = cols)+
-    ggplot2::labs(title = "Relative contact probability", x = "Distance (bp)", y = "RCP", col = '')+
-    ggplot2::coord_fixed(ratio = .75)
+    cols <- RCPdata$color
+    names(cols) <- RCPdata$sample
+    ggplot2::ggplot(RCPdata, ggplot2::aes(col = sample, x = distance/1e6, 
+        y = prob)) + ggplot2::geom_smooth(span = 0.25, se = F) + 
+        ggplot2::scale_x_log10() + ggplot2::scale_y_log10() + 
+        ggplot2::facet_wrap(~chrom, nrow = floor(sqrt(length(unique(RCP_WTWAPL$chrom))))) + 
+        ggplot2::theme_linedraw() + ggplot2::scale_color_manual(values = cols) + 
+        ggplot2::labs(title = "Relative contact probability", 
+            x = "Distance (Mbp)", y = "RCP", col = "") + ggplot2::theme(aspect.ratio = 1) + 
+      ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90,vjust = 0.5, hjust=1))
 }

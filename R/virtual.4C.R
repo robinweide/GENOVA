bc1e71d0328dbfb141066da15c09a7b4f9aa072e
Robin van der Weide <robinweide@gmail.com>
2016-10-28 11:35:27 +0200

    Update virtual.4C.R

--- R/virtual.4C.R
+++ R/virtual.4C.R
@@ -28,7 +28,7 @@ virtual.4C <- function (experiment, loop.bed, smallTreshold = 225000, verbose = 
     loop.bed <- na.exclude(loop.bed[1:6])
     loop.4c <- loop.bed[, c(1, 2, 6)]
     loop.4c$CHR1 <- gsub(loop.4c$CHR1, pattern = "chr ", replacement = "chr")
-    loop.4c.length <- length(loop.4c$V1)
+    loop.4c.length <- nrow(loop.4c)
     levels(loop.4c$V1) <- levels(bed$V1)
     results.vector <- rep(0, 100)
     SL <- 0

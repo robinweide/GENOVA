2b93a3dd63de159aa3e2641fb3045ac3562c02ec
Robin van der Weide <robinweide@gmail.com>
2016-10-28 11:52:52 +0200

    Update visualise.virtual.4C.R

--- R/visualise.virtual.4C.R
+++ R/visualise.virtual.4C.R
@@ -4,8 +4,8 @@
 #' @return A plot.
 #' @export
 visualise.virtual.4C <- function(data){
-  plot(data, type = 'l', ylim = c(min(data), max(data)*1.1), xlab = 'Normalised distance', ylab = 'Score per loop')
-  points(x = 31, y = max(data[25:35])+1,pch = 25, col = 4)
-  points(x = 73, y = max(data[70:80])+1,pch = 25, col = 2)
+  plot(data, type = 'l', ylim = c(min(data), max(data)*1.1), xlab = 'Normalised distance', ylab = 'Average score per loop')
+  points(x = 30, y = max(data[25:35])*1.05,pch = 25, col = 4)
+  points(x = 71, y = max(data[70:80])+(max(data[25:35])*.05),pch = 25, col = 2)
   legend(x = 80, y = max(data)*1.125,legend = c('Viewpoint', 'Anchor'), bty = 'n', col = c(4,2), pch = c(25,25))
 }

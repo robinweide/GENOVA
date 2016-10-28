2e27efeb9113df0a2639a74d18dab3c3a492d56e
Robin van der Weide <robinweide@gmail.com>
2016-10-28 11:02:09 +0200

    0.6.1

--- R/virtual.4C.R
+++ R/virtual.4C.R
@@ -9,7 +9,7 @@
 #' @param verbose Produces a progress-indication.
 #' @return A vector containing the y-axis scores per rescaled bin.
 #' @export
-function (experiment, loop.bed, smallTreshold = 225000, verbose = F) 
+virtual.4C <- function (experiment, loop.bed, smallTreshold = 225000, verbose = F)
 {
     hicdata <- experiment$ICE
     bed <- experiment$ABS
@@ -18,7 +18,7 @@ function (experiment, loop.bed, smallTreshold = 225000, verbose = F)
         data.table::setkey(hicdata, V1, V2)
     }
     bed.p <- paste0(bed[, 1], ":", bed[, 2])
-    loop.bed <- loop.bed[abs(loop.bed[, 6] - loop.bed[, 2]) >= 
+    loop.bed <- loop.bed[abs(loop.bed[, 6] - loop.bed[, 2]) >=
         smallTreshold, ]
     for (i in 1:length(loop.bed[, 1])) {
         if (loop.bed[i, 2] > loop.bed[i, 5]) {
@@ -34,7 +34,7 @@ function (experiment, loop.bed, smallTreshold = 225000, verbose = F)
     SL <- 0
     for (i in 1:loop.4c.length) {
         if (verbose) {
-            cat(paste0(i, " of ", loop.4c.length, " loops."), 
+            cat(paste0(i, " of ", loop.4c.length, " loops."),
                 "\r")
         }
         loopSize <- loop.4c[i, 3] - loop.4c[i, 2]
@@ -42,11 +42,11 @@ function (experiment, loop.bed, smallTreshold = 225000, verbose = F)
             next
         }
         halfLoopSize <- floor((loopSize/resolution)/1.5) * resolution
-        viewpoint <- paste0(loop.4c[i, 1], ":", as.integer(resolution * 
+        viewpoint <- paste0(loop.4c[i, 1], ":", as.integer(resolution *
             floor(loop.4c[i, 2]/resolution)))
-        start <- paste0(loop.4c[i, 1], ":", as.integer(resolution * 
+        start <- paste0(loop.4c[i, 1], ":", as.integer(resolution *
             floor((loop.4c[i, 2] - halfLoopSize)/resolution)))
-        end <- paste0(loop.4c[i, 1], ":", as.integer(resolution * 
+        end <- paste0(loop.4c[i, 1], ":", as.integer(resolution *
             floor((loop.4c[i, 3] + halfLoopSize)/resolution)))
         viewpoint <- gsub(" ", "", viewpoint, fixed = TRUE)
         start <- gsub(" ", "", start, fixed = TRUE)
@@ -54,7 +54,7 @@ function (experiment, loop.bed, smallTreshold = 225000, verbose = F)
         viewpoint.pos <- bed[match(viewpoint, bed.p), 4]
         start.pos <- bed[match(start, bed.p), 4]
         end.pos <- bed[match(end, bed.p), 4]
-        if (!all(is.finite(viewpoint.pos), is.finite(start.pos), 
+        if (!all(is.finite(viewpoint.pos), is.finite(start.pos),
             is.finite(end.pos))) {
             next
         }
@@ -68,10 +68,10 @@ function (experiment, loop.bed, smallTreshold = 225000, verbose = F)
         if (nrow(sel) < 10) {
             next
         }
-        missingX <- data.frame(start.pos:end.pos)[!data.frame(start.pos:end.pos)[, 
+        missingX <- data.frame(start.pos:end.pos)[!data.frame(start.pos:end.pos)[,
             1] %in% sel$V1, ]
         if (!is.null(dim(missingX))) {
-            missingX.DF <- data.frame(V1 = missingX, V2 = rep(viewpoint.pos, 
+            missingX.DF <- data.frame(V1 = missingX, V2 = rep(viewpoint.pos,
                 length(missingX)), V3 = 0)
             sel <- rbind(sel, missingX.DF)
         }

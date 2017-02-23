4b9953633669ab9542b5f6b1ce268a127c115bdc
Robin van der Weide <robinweide@gmail.com>
2017-02-23 12:44:00 +0100

    juicer-ready!

--- R/construct.experiment.R
+++ R/construct.experiment.R
@@ -1,30 +1,30 @@
 #' Construct a HiC-experiment.
-#'
+#' 
 #' Make a structure which holds the most needed information of a HiC-experiment.
-#'
-#' @param ICEDpath Full path to HiC-pro matrix-file.
-#' @param BEDpath Full path the HiC-pro index-file
-#' @param SAMPLENAME The name of the sample.
-#' @param COLOR Color associated with sample.
+#' 
+#' @param signalPath Full path to a HiC-pro-like matrix-file or .sig from juicerToGenova.py
+#' @param indicesPath Full path the HiC-pro-like index-file or .bed from juicerToGenova.py
+#' @param name The name of the sample.
+#' @param color Color associated with sample.
 #' @param COMMENTS A place to store some comments.
 #' @return A list.
 #' @export
-construct.experiment <- function(ICEDpath,BEDpath, SAMPLENAME, COLOR = 1, COMMENTS = NULL){
+construct.experiment <- function(signalPath, indicesPath, name, color = 1, comments = NULL){
   # Check if files exist
-  if(!file.exists(ICEDpath)){stop('ICE-matrix file not found.')}
-  if(!file.exists(BEDpath)){stop('ICE-index file not found.')}
+  if(!file.exists(signalPath)){stop('ICE-matrix file not found.')}
+  if(!file.exists(indicesPath)){stop('ICE-index file not found.')}
 
-  ICE <- read.hicpro.matrix(ICEDpath)
-  ABS <- data.table::fread(BEDpath, header = F, data.table = F)
+  ICE <- read.hicpro.matrix(signalPath)
+  ABS <- data.table::fread(indicesPath, header = F, data.table = F)
 
   # check for similar binsizes BED and ICE
   ICEmaxID <- max(max(ICE$V1), max(ICE$V2))
   ABSmaxID <- max(ABS[,4])
   if( ICEmaxID > ABSmaxID){
-    stop("Undefined HiC-bins.\nWe checked if the highest Hi-C bin in the Matrix-file could be found in the BED-file: it couldn't.\nPlease check if BED and Matrix are from the same reference genome and resolution!")
+    stop("Undefined HiC-bins.\nWe checked if the highest Hi-C bin in the signal-file could be found in the indices-file: it couldn't.\nPlease check if indices and signal are from the same reference genome and resolution!")
   }
   if( abs(ICEmaxID - ABSmaxID) > ICEmaxID*0.1 ) {
-    warning("Undefined BED-bins.\nWe checked if the highest Hi-C bin in the BED-file could be found in the Matrix-file: it couldn't.\nPlease check if BED and Matrix are from the same reference genome and resolution!")
+    warning("Undefined BED-bins.\nWe checked if the highest Hi-C bin in the indices-file could be found in the signal-file: it couldn't.\nPlease check if indices and signal are from the same reference genome and resolution!")
   }
   chromVector <- as.character(unique(ABS$V1))
   res = as.numeric( median(ABS$V3-ABS$V2)  )
@@ -38,7 +38,7 @@ construct.experiment <- function(ICEDpath,BEDpath, SAMPLENAME, COLOR = 1, COMMEN
     ABS = ABS,
 
     # Name of sample
-    NAME = SAMPLENAME,
+    NAME = name,
 
     # Resolution of sample
     RES = res,
@@ -46,11 +46,11 @@ construct.experiment <- function(ICEDpath,BEDpath, SAMPLENAME, COLOR = 1, COMMEN
     # Available chromosomes
     CHRS = chromVector,
 
-    # Color of sample (optional, but recommended for running RCP)
-    COL = COLOR,
+    # color of sample (optional, but recommended for running RCP)
+    COL = color,
 
-    # Comments
-    COMM = COMMENTS,
+    # comments
+    COMM = comments,
 
     # Vector of masked bins
     MASK = vector()

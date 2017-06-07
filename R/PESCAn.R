830654f3fdc55511efbd7e490fe54de340c79662
Robin van der Weide <robinweide@gmail.com>
2017-06-07 12:06:22 +0200

    0.9.2
    PE-SCAn

--- R/PESCAn.R
+++ R/PESCAn.R
@@ -6,11 +6,21 @@
 #' @param bed A bed-dataframe.
 #' @param minDist The minimal distance
 #' @param add Add constant value to bed-start and -end.
-#' @param size The amount of Hi-C bins to take into account (i.e. a score of 21 yield an output with 10 Hi-C bins up- and downstream of the anchor). 
+#' @param size The amount of Hi-C bins to take into account (i.e. a score of 21 yield an output with 10 Hi-C bins up- and downstream of the anchor).
 #' @return A score-matrix.
 #' @import data.table
+#' @examples
+#' # Run PE-SCAn on a bed of super-enhancers, using WT Hi-C data.
+#' SE <- read.delim(superEnhancers.bed, header = F)
+#' SE_vs_WT <- PESCAn(experiment = WT, bed = SE, size = 500e3)
+#'
+#' # Do a circular perutation by adding 1Mb.
+#' SE_vs_WT_perm <- PESCAn(experiment = WT, bed = SE, size = 500e3, add = 1e6)
+#'
+#' # Plot using, for example, persp
+#' persp(SE_vs_WT/SE_vs_WT_perm, phi = 30, theta = 30, col = 'skyblue')
 #' @export
-#' 
+#'
 PESCAn <- function( experiment, bed, minDist = 5e6, size = 500e3, add = 0 ){
   #sorting the bed file is essential for the analysis
   bed <- bed[order(bed[,1],bed[,2]),]
@@ -26,5 +36,5 @@ PESCAn <- function( experiment, bed, minDist = 5e6, size = 500e3, add = 0 ){
       count = pe.res$count
     }
   }
-  score.mat/count
-}  
+  return(score.mat/count)
+}
--- R/cov2d.R
+++ R/cov2d.R
@@ -6,54 +6,77 @@
 #' @param bed A bed-dataframe.
 #' @param minDist The minimal distance
 #' @param add Add constant value to bed-start and -end.
-#' @param size The amount of Hi-C bins to take into account (i.e. a score of 21 yield an output with 10 Hi-C bins up- and downstream of the anchor). 
+#' @param size The amount of Hi-C bins to take into account (i.e. a score of 21 yield an output with 10 Hi-C bins up- and downstream of the anchor).
+#' @param outlierCutOff The severity of outliers: roughly translates to the amount of MADs above the median.
 #' @return A list containing a score-matrix and a count-variable of the amount of used bed-entries.
 #' @import data.table
-cov2d <- function( experiment, bed, minDist=5e6, size=500e3, add=0){
+cov2d <- function( experiment, bed, minDist=5e6, size=500e3, add=0, outlierCutOff = 40){
   #read data from experiment list
   data = experiment$ICE
-  window = experiment$RES 
+  window = experiment$RES
   idx = experiment$ABS
-  
+
   minDist <- minDist/window
   middle <- (bed[,2]+bed[,3])/2
   bed[,2] <- middle-size+add
   bed[,3] <- middle+size+add
-  
+
   p1 <- paste0(bed[,1], ":", as.integer(window*floor(bed[,2]/window)) )
   p2 <- paste0(bed[,1], ":", as.integer(window*floor(bed[,3]/window)) )
   idx.p <- paste0(idx[,1], ":", idx[,2])
   start <- idx[match(p1,idx.p),4]
   end   <- idx[match(p2,idx.p),4]
-  
+
   #make sure the bed positions are found in the genome/Hi-C pro file
   sel <- !is.na(start) & !is.na(end)
+  # cat(sel,'\n')
   bed <- bed[sel,]
   start <- start[sel]
   end <- end[sel]
   score.matrix <- matrix(0, ncol=max(end-start)+1, nrow=max(end-start)+1)
   count <- 0
+  rawMatList <- list()
   #double looping over the data
   for( i in 1:(length(start)-1)){
     #cat(i, "\r")
     for( j in (i+1):length(start)){
       if(start[j]-start[i] < minDist)
         next
-      
+
       x <- rep(start[i]:end[i],end[j]-start[j]+1)
       y <- rep(start[j]:end[j],each=end[i]-start[i]+1)
-      
+
       s <- data[list(x,y)]
-      
-      #NOTE! this is currently hard-coded, how can we make this a dynamic value based
-      #on the number of reads we normalize to (this should then be stored in the experiment list)
-      s <- s[!is.na(s$V3) & s$V3 < 10]
-      
-      #create a sub matrix to add to the actual matrix
-      score.matrix[cbind(s$V1-start[i]+1,s$V2-start[j]+1)] <- score.matrix[cbind(s$V1-start[i]+1,s$V2-start[j]+1)] + s$V3
+      s.mat <- reshape2::acast(s, V1~V2, value.var="V3")
+
+      # Remove NA
+      s.mat[is.na(s.mat)] <- 0
+
+      # Add to rawMatList
+      rawMatList[[paste(i,j,sep = "_")]] <- s.mat
+
       count <- count+1
-    }       
-    
+    }
+
   }
-  list(score=score.matrix, count=count)
+
+  rawMatList <- rawMatList[!unlist(lapply(rawMatList, is.null))]
+  sm <- simplify2array(rawMatList)
+  sm[sm == 0] <- NA
+  MED <- apply(sm,MARGIN = 1:2, FUN = function(x) median(x,na.rm = T))
+  MAD <- apply(sm,MARGIN = 1:2, FUN = function(x) mad(x,na.rm = T))
+  tres <- MED+(MAD*outlierCutOff)
+  tres[is.na(tres)] <- 0
+  for(i in 1:length(rawMatList)){
+    #rawMatList[[i]][rawMatList[[i]][1:99,1:99] > tres[1:99,1:99]] <- 0 #tres[rawMatList[[i]][1:99,1:99] > tres[1:99,1:99]]
+    m <- rawMatList[[i]]
+    #cat(i, "\n")
+    if(any(m > tres)){
+      rawMatList[[i]] <- matrix(0, nrow = dim(sm)[1], ncol=dim(sm)[2])
+      count <- count - 1
+    }
+  }
+  STACKoutlierr <- Reduce(rawMatList, f = '+')
+
+  return(list(score=STACKoutlierr, count=count))
 }

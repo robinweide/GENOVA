4387adab04109c66d9836de0d0191ee1f48db989
Robin Van der Weide <r.vd.weide@shannon.rhpc.nki.nl>
2018-06-11 11:06:20 +0200

    11.6.18: quantifyAPA w/ donut-enrichment (log2)

--- R/quantifyAPA.R
+++ R/quantifyAPA.R
@@ -9,6 +9,24 @@ getPixelMean <- function(MAT, pixWidth = 3, npix, NAasZero = T){
 }
 
 
+getDonutMean <- function(MAT, pixWidth = 3, npix, NAasZero = T){
+  pixLow  <- ((npix-1 ) /2 ) - ((pixWidth-1 ) /2 )
+  pixHigh <- ((npix-1 ) /2 ) + ((pixWidth-1 ) /2 )
+
+  donutLow  <- pixLow - pixWidth
+  donutHigh <- pixHigh + pixWidth
+
+  subMat <- MAT[donutLow:donutHigh, donutLow:donutHigh]
+  if(NAasZero){
+    subMat[is.na(subMat)] <- 0
+  }
+
+  subMat[(pixWidth+1):(pixWidth*2),(pixWidth+1):(pixWidth*2)] <- NA
+
+
+  return(mean(subMat, na.rm = T))
+}
+
 #' Get statistics from the centers of APA-results
 #'
 #' Takes the input from APA() and produces a list of average centers and
@@ -17,6 +35,7 @@ getPixelMean <- function(MAT, pixWidth = 3, npix, NAasZero = T){
 #'
 #' @param APAlist A named list of outputs from APA().
 #' @param pixWidth The width of the square to use. 1 will give you just the
+#' @param enrichment Calculate log2-enrichment instead of contacts?
 #' center-point
 #' @return Alist of two dataframes: data (per-loop and -sample average scores)
 #' and stats (all-vs-all wilcoxon.test p-values and log2-foldchanges.)
@@ -35,15 +54,31 @@ getPixelMean <- function(MAT, pixWidth = 3, npix, NAasZero = T){
 #' ggplot(APAstats$data, aes(x = sample, y = value)) +
 #'   geom_boxplot()
 #' @export
-quantifyAPA <- function(APAlist, pixWidth = 3){
+quantifyAPA <- function(APAlist, enrichment = F, pixWidth = 3, speudoCount = 1){
 
   outDF <- list() # make a df with a line per loop (add column for color and name)
 
   for(i in 1:length(APAlist)){
     apaout <- APAlist[[i]]$rawMatList
     npix   <- nrow(apaout[[1]])
-    avgs   <- lapply(apaout, getPixelMean, pixWidth = pixWidth, npix = npix)
-    avgs   <- unlist(avgs)
+    avgsPix   <- lapply(apaout, getPixelMean, pixWidth = pixWidth, npix = npix)
+    avgsPix   <- unlist(avgsPix)
+
+    avgs <- NULL
+    if(enrichment){
+      tmp = unlist(lapply(apaout, min))
+      if(is.null(speudoCount)){
+        speudoCount = min(tmp[tmp != 0])
+      }
+      avgsDonut   <- lapply(apaout, getDonutMean, pixWidth = pixWidth, npix = npix)
+      avgsDonut   <- unlist(avgsDonut)
+
+      avgs <- log2((speudoCount+avgsPix)/(speudoCount + avgsDonut))
+    } else {
+      avgs <- avgsPix
+    }
+
+
     df     <- data.frame(sample = names(APAlist)[i],
                          loopIDX = 1:length(apaout),
                          value = avgs)

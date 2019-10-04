866e2f9c4156810a335772378c24f94398f45bd3
teunbrand <tahvdbrand@gmail.com>
2019-10-04 13:25:43 +0200

    Refactored centromere functions.

--- R/load_contacts.R
+++ R/load_contacts.R
@@ -283,48 +283,6 @@ zscore_hic = function(SIG, ABS){
   return(Z)
 }
 
-clean_centromeres <- function(centros, resolution) {
-  # Essentially does the same as `reduce(a_granges_object, min.gapwidth = resolution)
-  
-  centros <- as.data.frame(centros)
-  
-  # Drop unused factor levels
-  if (is.factor(centros[, 1])) {
-    centros[, 1] <- droplevels(centros[, 1])
-  }
-  
-  # Set column names and order on chromosome and start
-  centros <- setNames(centros, paste0("V", 1:3))
-  centros <- centros[order(centros[, 1], centros[, 2]), ]
-  
-  # Test wether adjacent entries should be merged
-  centros$merge <- c(FALSE, vapply(seq_len(nrow(centros) - 1), function(i) {
-    
-    # Is the difference between end and next start sub-resolution?
-    test <- abs(centros[i, 3] - centros[i + 1, 2]) < resolution
-    
-    # Are the entries on the same chromosome?
-    test && centros[i, 1] == centros[i + 1, 1]
-  }, logical(1)))
-  
-  # Merge the TRUE entries
-  while (any(centros$merge)) {
-    # What is the next entry to be merged?
-    i <- which(centros$merge)[1]
-    
-    # Replace end of previous entry with end of current entry
-    centros[i - 1, 3] <- centros[i, 3]
-    
-    # Remove current entry
-    centros <- centros[-i, ]
-  }
-  
-  # Return centromeres
-  centros[, 1:3]
-}
-
-
-
 legacy_out = function(out){
   newOut = list(
     # Iced HiC-matrix in three-column format (i.e. from HiC-pro)
